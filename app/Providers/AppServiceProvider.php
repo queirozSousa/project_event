5bf02c35d8d22eca9468c55db65004d09c42b30b
Queiroz Sousa <queirozmacamo82@gmail.com>
2021-06-23 12:00:27 +0200

    Update AppServiceProvider.php
    third commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if(env('REDIRECT_HTTPS')){
-            $this->app['request']->server->set('HTTPS', true);
-        }
+        Schema::defaultStringLength(191);
     }
 
     /**
@@ -23,10 +21,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('REDIRECT_HTTPS')){
-            $url->formatScheme('https');
-        }
+        //
     }
 }
